4a29b17e75ee6cf94e7a3665ddb23ffc94bf65cd
B0X12 <box0_@naver.com>
2023-11-28 13:56:49 +0900

    🧠 refactor: 태그 정렬 순서 수정

--- src/containers/Feed/components/lists/TagList.tsx
+++ src/containers/Feed/components/lists/TagList.tsx
@@ -32,11 +32,22 @@ const TagList: React.FC<Props> = ({ className, data }) => {
     }
   }
 
+  const order = [
+    "𝘏𝘦𝘭𝘭𝘰! 👋🏻",
+    "𝘑𝘢𝘷𝘢 & 𝘚𝘱𝘳𝘪𝘯𝘨",
+    "𝘋𝘢𝘵𝘢𝘣𝘢𝘴𝘦",
+    "𝘋𝘦𝘷𝘖𝘱𝘴",
+    "𝘎𝘪𝘵",
+    "𝘐𝘴𝘴𝘶𝘦",
+    "𝘚𝘪𝘥𝘦-𝘗𝘳𝘰𝘫𝘦𝘤𝘵",
+    "𝘌𝘵𝘤"
+  ];
+
   return (
     <div className={className}>
       <div className="hidden lg:block p-1 mb-3 text-black dark:text-white">🏷 ᴛᴀɢs</div>
       <ul className="gap-1 flex mobile-x-scroll lg:block mb-6">
-        {Object.keys(data).map((key) => (
+        {order.map((key) => (
           <li
             key={key}
             className={`cursor-pointer text-sm p-1 px-4 my-1 flex-shrink-0 rounded-xl text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-black-80 ${
@@ -50,6 +61,7 @@ const TagList: React.FC<Props> = ({ className, data }) => {
         ))}
       </ul>
     </div>
+
   )
 }
 
